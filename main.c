10844b1d24c6fe647afb644162b37b72e21b8d36
NoNameAlien <88541451+NoNameAlien@users.noreply.github.com>
2022-08-07 14:36:34 +0700

    V1.3

--- main.c
+++ main.c
@@ -2,33 +2,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 #define rows 25
-#define columns 80
+#define columns 30
 
 void begin();
 char *create();
 double getRandomDoubleInRange(double min, double max);
 void display(char *simulation);
+int countNeighborhood(int x, int y, char *simulation);
+char *step(char *prevSimulation);
 
 int main() {
   initscr();
   noecho();
   raw();
   //   while (1) {
-  halfdelay(1);
+  //   halfdelay(1);
 
   begin();
 
-  refresh();
-  delay_output(100);
-  clear();
+  //   refresh();
+  //   delay_output(100);
+  //   clear();
   // }
 }
 
 void begin() {
-  printw("Begin!");
+  printw("Begin!\n");
   char *simulation = create();
   if (simulation == NULL) return;
   display(simulation);
+
+  while (1) {
+    halfdelay(1);
+
+    char *newSim = step(simulation);
+    if (newSim == NULL) return;
+
+    free(simulation);
+    simulation = newSim;
+    display(simulation);
+
+    refresh();
+    delay_output(1000);
+    clear();
+  }
 }
 
 char *create() {
@@ -50,11 +67,50 @@ double getRandomDoubleInRange(double min, double max) {
 }
 
 void display(char *simulation) {
-  printw("\n\n\n\n\n");
   for (int i = 0; i < rows; i++) {
     for (int j = 0; j < columns; j++) {
       printw("%c ", *(simulation + i * columns + j));
     }
     printw("\n");
   }
+}
+
+int countNeighborhood(int x, int y, char *simulation) {
+  int count = 0;
+  int pivot = y * columns + x;
+
+  for (int i = -1; i <= 1; i++) {
+    for (int j = -1; j <= 1; j++) {
+      char c = *(simulation + pivot + (i * columns) + j);
+
+      if (c == '#') count++;
+    }
+  }
+  return count;
+}
+
+char *step(char *prevSimulation) {
+  char *steppedSimulation = calloc(rows * columns, sizeof(int));
+
+  if (steppedSimulation == NULL) return NULL;
+
+  for (int i = 0; i < rows; i++) {
+    for (int j = 0; j < columns; j++) {
+      int live = countNeighborhood(j, i, prevSimulation);
+      char cell = *(prevSimulation + i * columns + i);
+      if (cell == '#') live--;
+      *(steppedSimulation + i * columns + i) = cell;
+
+      if (live < 2) {
+        *(steppedSimulation + i * columns + j) = '.';
+      } else if ((live == 2 || live == 3) && cell == '#') {
+        *(steppedSimulation + i * columns + j) = '#';
+      } else if (live > 3 && cell == '#') {
+        *(steppedSimulation + i * columns + j) = '.';
+      } else if (live > 3 && cell == '.') {
+        *(steppedSimulation + i * columns + j) = '#';
+      }
+    }
+  }
+  return steppedSimulation;
 }
